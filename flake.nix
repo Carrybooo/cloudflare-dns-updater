4d51302167da2fb5c34b499defea5078de54a579
Valentin Guerlesquin <46242209+Carrybooo@users.noreply.github.com>
2025-01-01 18:30:10 +0100

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A flake for the DNS updater project";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.cloudflare-dns-updater = let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+    in pkgs.rustPlatform.buildRustPackage rec {
+      pname = "cloudflare-dns-updater";
+      version = "0.1.0";
+
+      # Replace with your GitHub repo details
+      src = pkgs.fetchFromGitHub {
+        owner = "Carrybooo";
+        repo = "cloudflare-dns-updater";
+        rev = "v${version}";
+        hash = "sha256-3P7o70WEyQl2DyiKgKdMOuRgCa9y5Ryp8hVo1AY0seo=";
+      };
+
+      nativeBuildInputs = [ pkgs.pkg-config ];
+      buildInputs = [ pkgs.openssl ];
+
+      configurePhase = ''
+        export PKG_CONFIG_PATH=${pkgs.openssl.dev}/lib/pkgconfig
+      '';
+
+      cargoHash = "sha256-lW4pqPm79u4baoahVGjTw9dyc6bG3bSAj9hf0NZj+DY=";
+
+      meta = with pkgs.lib; {
+        description = "A DNS updater written in Rust";
+        homepage = "https://github.com/Carrybooo/cloudflare-dns-updater";
+        license = licenses.mit;
+      };
+    };
+  };
+}
